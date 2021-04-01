ca1ccb25aea6a47ba613c844950896dafc450a42
Anuj kumar <anujdwivedi88699@gmail.com>
2021-04-01 20:14:43 +0530

    third

--- app.js
+++ app.js
@@ -9,8 +9,11 @@ mongoose.connect('mongodb+srv://anuj:anuj@cluster0.5vava.mongodb.net/myFirstData
 
 app.use(express.json());
 app.use(require('./routes/assignment.route'))
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
-
-app.listen(5000,function(){
+app.listen(process.env.PORT ||5000,function(){
   console.log("server running on port 5000.");
 });
